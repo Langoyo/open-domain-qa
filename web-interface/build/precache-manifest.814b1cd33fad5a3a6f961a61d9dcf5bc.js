44d607a081b17c2038cb4f131acee689b1d8506a
Andrés Langoyo <alangoyom01@gmail.com>
2023-06-11 16:48:10 +0200

    build

---
+++ web-interface/build/precache-manifest.814b1cd33fad5a3a6f961a61d9dcf5bc.js
@@ -0,0 +1,86 @@
+self.__precacheManifest = [
+  {
+    "revision": "0e6e24476fb2faebeb17",
+    "url": "/static/css/main.ec1b559d.chunk.css"
+  },
+  {
+    "revision": "0e6e24476fb2faebeb17",
+    "url": "/static/js/main.c61c5f8f.chunk.js"
+  },
+  {
+    "revision": "2f06dec600ee59d10425",
+    "url": "/static/js/runtime~main.9b03e1a2.js"
+  },
+  {
+    "revision": "4db7f34bb20b7e48c743",
+    "url": "/static/css/2.e6db85d6.chunk.css"
+  },
+  {
+    "revision": "4db7f34bb20b7e48c743",
+    "url": "/static/js/2.9fd0881a.chunk.js"
+  },
+  {
+    "revision": "c037fbb12dd69d54d2cd",
+    "url": "/static/js/3.1f812985.chunk.js"
+  },
+  {
+    "revision": "719a3925c2f92e6a9aba3aaeac28d9a0",
+    "url": "/static/media/wiki.719a3925.jpg"
+  },
+  {
+    "revision": "d211c298f5dddd6d141ac9ebee92d8a1",
+    "url": "/static/media/img-8.d211c298.jpg"
+  },
+  {
+    "revision": "cd1d1c978d0030f14f1d8a7690eb8398",
+    "url": "/static/media/img-1.cd1d1c97.jpg"
+  },
+  {
+    "revision": "4143d240af520e4a8b565060c6e6277b",
+    "url": "/static/media/img-2.4143d240.jpg"
+  },
+  {
+    "revision": "aac0dfefc080856931658ea9c760534e",
+    "url": "/static/media/united-states.aac0dfef.png"
+  },
+  {
+    "revision": "69bf7c3df4b222c445bf6ebffec278e6",
+    "url": "/static/media/spain.69bf7c3d.png"
+  },
+  {
+    "revision": "daca0e7afd0299f673226c9b8b07de8d",
+    "url": "/static/media/chat.daca0e7a.jpg"
+  },
+  {
+    "revision": "dc357dab8c58930abe521bc0f1934dae",
+    "url": "/static/media/table.dc357dab.jpg"
+  },
+  {
+    "revision": "08077dfc7ec5524a49de95a7df32206d",
+    "url": "/static/media/advanced.08077dfc.jpg"
+  },
+  {
+    "revision": "c46aac71f2260b30c274f41709776ca3",
+    "url": "/static/media/profile.c46aac71.png"
+  },
+  {
+    "revision": "72ffeebe4ec0651990663d6ed66af3e7",
+    "url": "/static/media/st.72ffeebe.jpg"
+  },
+  {
+    "revision": "245897b62f547d66c1208e67ae927a35",
+    "url": "/static/media/bert.245897b6.jpg"
+  },
+  {
+    "revision": "2009a6353d8e4feda87fe9e1b109f192",
+    "url": "/static/media/drqa.2009a635.jpg"
+  },
+  {
+    "revision": "682abe2c4f806c5231823f37f50a1ad8",
+    "url": "/static/media/favicon.682abe2c.ico"
+  },
+  {
+    "revision": "45b5f18e23ffb4f8290be6a345d0f529",
+    "url": "/index.html"
+  }
+];
